a448428917c1131fa47f8471305bb621d5b32733
Scarlett Vu <uscarlettv@gmail.com>
2023-02-15 02:11:10 -0800

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Scarlett  on 2/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
